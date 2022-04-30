b03b2b6a2ff09d2a2937915228a09b8daf96f745
lixiang16 <lixiang16@corp.netease.com>
2022-04-30 19:41:23 +0800

    fix: fix style

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -5,13 +5,13 @@
       <li class="list-inline-item"><router-link to="/login" class="btn btn-outline-light my-2">登陆</router-link></li>
       <li class="list-inline-item"><router-link to="/signup" class="btn btn-outline-light my-2">注册</router-link></li>
     </ul>
-    <ul v-else class="list-inline mb-0">
+    <ul v-else class="list-inline">
       <li class="list-inline-item">
         <Dropdown :title="`你好 ${user.name}`">
-          <dropdown-item><router-link to="/create" class="dropdown-item">新建文章</router-link></dropdown-item>
-          <!-- <dropdown-item><router-link :to="`/column/${user.column}`" class="dropdown-item">我的专栏</router-link></dropdown-item> -->
-          <dropdown-item disabled><a href="#" class="dropdown-item">编辑资料</a></dropdown-item>
-          <dropdown-item><a href="#" class="dropdown-item">退出登陆</a></dropdown-item>
+          <DropdownItem><router-link to="/create" class="dropdown-item">新建文章</router-link></DropdownItem>
+          <!-- <DropdownItem><router-link :to="`/column/${user.column}`" class="DropdownItem">我的专栏</router-link></DropdownItem> -->
+          <DropdownItem disabled><a href="#" class="dropdown-item">编辑资料</a></DropdownItem>
+          <DropdownItem><a href="#" class="dropdown-item">退出登陆</a></DropdownItem>
         </Dropdown>
       </li>
     </ul>
@@ -21,6 +21,7 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import Dropdown from './Dropdown.vue'
+import DropdownItem from './DropdownItem.vue'
 
 export interface UserProps {
   isLogin: boolean
@@ -30,7 +31,7 @@ export interface UserProps {
 
 export default defineComponent({
   name: 'GlobalHeader',
-  components: { Dropdown },
+  components: { Dropdown, DropdownItem },
   props: {
     user: {
       type: Object as PropType<UserProps>,
