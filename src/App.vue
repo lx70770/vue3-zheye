714933b7fa19600a96fcb0534b76d1514acfb1f8
lixiang16 <lixiang16@corp.netease.com>
2022-04-30 18:44:30 +0800

    feat: init&add commit lint

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <GlobalHeader :user="userData" />
+    <ColumnList :lists="list" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import GlobalHeader from './components/GlobalHeader.vue'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './global.css'
+
+const testData: Array<ColumnProps> = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '描述1',
+    avatar: 'https://images.dog.ceo/breeds/bullterrier-staffordshire/n02093256_5139.jpg'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '描述2',
+    avatar: 'https://images.dog.ceo/breeds/clumber/n02101556_5137.jpg'
+  },
+  {
+    id: 3,
+    title: 'test2的专栏',
+    description: '描述2',
+    avatar: 'https://images.dog.ceo/breeds/clumber/n02101556_5137.jpg'
+  },
+  {
+    id: 4,
+    title: 'test2的专栏',
+    description: '描述2',
+    avatar: 'https://images.dog.ceo/breeds/clumber/n02101556_5137.jpg'
+  },
+  {
+    id: 5,
+    title: 'test2的专栏',
+    description: '描述2',
+    avatar: 'https://images.dog.ceo/breeds/clumber/n02101556_5137.jpg'
+  }
+]
+
+const userData = {
+  isLogin: true,
+  name: 'lx70770',
+  id: '12122'
+}
+
+export default defineComponent({
+  name: 'App',
+  components: { ColumnList, GlobalHeader },
+  setup() {
+    return {
+      list: testData,
+      userData
+    }
+  }
+})
+</script>
+
+<style></style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="row">
+    <div v-for="list in lists" :key="list.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="list.avatar" :alt="list.title" class="card-avatar-img" />
+          <h5 class="card-title">{{ list.title }}</h5>
+          <p class="card-text text-left">{{ list.description }}</p>
+          <a href="#" class="btn btn-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export interface ColumnProps {
+  id: number
+  title: string
+  avatar: string
+  description: string
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    lists: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+.card-avatar-img {
+  width: 80px;
+  height: 80px;
+  object-fit: cover;
+  border-radius: 50%;
+  margin-bottom: 15px;
+}
+</style>
